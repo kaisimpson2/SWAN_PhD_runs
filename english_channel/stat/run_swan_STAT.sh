c9e220317b6dea5a02892ad51bee799e9e71a972
Kai Simpson <hsksimp2@viz2.barkla2.liv.alces.network>
2026-04-28 11:37:38 +0100

    added git ignore, outputed files from SWAN

--- english_channel/stat/run_swan_STAT.sh
+++ english_channel/stat/run_swan_STAT.sh
@@ -4,11 +4,11 @@
 #SBATCH -N 1
 #SBATCH -n 64
 #SBATCH -t 02:00:00
-#SBATCH -o ~/scratch/swan/runs/english_channel/stat/swan_STAT_%j.out
-#SBATCH -e ~/scratch/swan/runs/english_channel/stat/swan_STAT_%j.err
+#SBATCH -o /users/hsksimp2/scratch/swan/runs/english_channel/stat/swan_STAT_%j.out
+#SBATCH -e /users/hsksimp2/scratch/swan/runs/english_channel/stat/swan_STAT_%j.err
 #SBATCH --mail-user=hsksimp2@liverpool.ac.uk
 #SBATCH --mail-type=ALL
-#SBATCH -D ~/scratch/swan/runs/english_channel/stat/
+#SBATCH -D /users/hsksimp2/scratch/swan/runs/english_channel/stat/
 
 module purge
 module load openmpi/5.0.8-gcc14.2.0
@@ -17,10 +17,10 @@ export OMP_NUM_THREADS=1
 export OMPI_MCA_mtl=^psm2
 export OMPI_MCA_btl=^openib
 
-cd ~/scratch/swan/runs/english_channel/stat/
+cd /users/hsksimp2/scratch/swan/runs/english_channel/stat/
 
 cp english_channel_STAT.swn INPUT
 
-mpirun -np $SLURM_NTASKS ~/scratch/swan/swan.exe
+mpirun -np $SLURM_NTASKS /users/hsksimp2/scratch/swan/swan.exe
 
 mv INPUT english_channel_STAT.swn
